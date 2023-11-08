e6c006899cce39ebf9944571dcb3bcdf3aa09f14
Timothy Marriott <timothyrmarriott@gmail.com>
2023-11-08 23:42:22 +0800

    Commit Source Code

---
+++ DVD/Program.cs
@@ -0,0 +1,85 @@
+﻿using Raylib_cs;
+
+public class DVD{
+
+    public static DVD Master;
+
+    public Vector2I WindowSize = new Vector2I(1920/2, 540);
+
+    public Vector2 ImagePosition;
+    public Vector2 ImageSpeed = new Vector2(0.125f, 0.125f);
+    public Vector2 ImageSize => new Vector2(LogoTexture.Width, LogoTexture.Height) * ImageScale;
+
+    public Vector2 ImageBottomRight => ImagePosition + ImageSize;
+    public Vector2 ImageBottomLeft => ImagePosition + ImageSize * new Vector2(1, 0);
+    public Vector2 ImageTopRight => ImagePosition + ImageSize * new Vector2(0, 1);
+    public Vector2 ImageTopLeft => ImagePosition;
+
+    public Texture2D LogoTexture;
+
+    public float ImageScale = 0.125f;
+
+    public float Time;
+
+    public static void Main(){
+        new DVD().Init();
+    }
+
+    public void Init(){
+        Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_HIGHDPI | ConfigFlags.FLAG_WINDOW_UNDECORATED | ConfigFlags.FLAG_WINDOW_TRANSPARENT | ConfigFlags.FLAG_WINDOW_ALWAYS_RUN | ConfigFlags.FLAG_WINDOW_UNFOCUSED | ConfigFlags.FLAG_WINDOW_TOPMOST | ConfigFlags.FLAG_WINDOW_MOUSE_PASSTHROUGH);
+        Raylib.InitWindow(WindowSize.x, WindowSize.y, $"DVD - {WindowSize.x}, {WindowSize.y} - 0");
+
+        Start();
+
+        while(!Raylib.WindowShouldClose()){
+
+            WindowSize.x = Raylib.GetScreenWidth();
+            WindowSize.y = Raylib.GetScreenHeight();
+            Raylib.SetWindowTitle($"DVD - {WindowSize.x}, {WindowSize.y} - {Raylib.GetFPS()}");
+
+            Raylib.BeginDrawing();
+            
+            Raylib.ClearBackground(Color.BLANK);
+
+            Time += Raylib.GetFrameTime();
+
+            Draw();
+
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    
+    }
+
+    public void Start(){
+        LogoTexture = Raylib.LoadTexture(AppDomain.CurrentDomain.BaseDirectory + "/Logo.png");
+        ImagePosition = new Vector2(Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor()) / 2 , Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor()) / 2) - ImageSize / 2;
+        int xScale = Raylib.GetRandomValue(0, 1);
+        int yScale = Raylib.GetRandomValue(0, 1);
+        ImageSpeed *= new Vector2(xScale == 0 ? -1 : 1, yScale == 0 ? -1 : 1);
+    }
+
+    public void Draw(){
+        
+        float r = ((MathF.Sin(Time) + 1) / 2) * 255;
+        float g = ((MathF.Sin(Time + 2345) + 1) / 2) * 255;
+        float b = 0;
+
+        Raylib.DrawTextureEx(LogoTexture, new Vector2(0, 0), 0, ImageScale, new Color((byte)r, (byte)g, (byte)b, (byte)255));
+
+
+        if (ImageTopLeft.x <= 0) ImageSpeed.x *= -1;
+        if (ImageTopLeft.y <= 0) ImageSpeed.y *= -1;
+        if (ImageBottomRight.y >= Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor())) ImageSpeed.y *= -1;
+        if (ImageBottomRight.x >= Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor())) ImageSpeed.x *= -1;
+
+        ImagePosition += ImageSpeed;
+
+        Raylib.SetWindowSize((int)ImageSize.x, (int)ImageSize.y);
+
+        Raylib.SetWindowPosition((int)MathF.Round(ImagePosition.x), (int)MathF.Round(ImagePosition.y));
+    }
+
+}
---
+++ DVD/Vector2.cs
@@ -0,0 +1,152 @@
+﻿
+
+public class Vector2
+{
+
+    public float x { get; set; }
+    public float y { get; set; }
+
+    public Vector2(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float Magnitude()
+    {
+        return (float)Math.Sqrt(x * x + y * y);
+    }
+
+    public void Normalize()
+    {
+        float magnitude = Magnitude();
+        if (magnitude > 0)
+        {
+            x /= magnitude;
+            y /= magnitude;
+        }
+    }
+
+    public static Vector2 operator +(Vector2 a, Vector2 b)
+    {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    }
+
+    public static Vector2 operator -(Vector2 a, Vector2 b)
+    {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    }
+
+    public static Vector2 operator *(Vector2 a, float scalar)
+    {
+        return new Vector2(a.x * scalar, a.y * scalar);
+    }
+
+    public static Vector2 operator *(Vector2 a, Vector2 scalar)
+    {
+        return new Vector2(a.x * scalar.x, a.y * scalar.y);
+    }
+
+    public static Vector2 operator /(Vector2 a, float scalar)
+    {
+        if (scalar != 0)
+        {
+            return new Vector2(a.x / scalar, a.y / scalar);
+        }
+        throw new DivideByZeroException("Division by zero is not allowed.");
+    }
+
+    public static implicit operator System.Numerics.Vector2(Vector2 customVector)
+    {
+        return new System.Numerics.Vector2(customVector.x, customVector.y);
+    }
+
+    public static implicit operator Vector2(System.Numerics.Vector2 customVector)
+    {
+        return new Vector2(customVector.X, customVector.Y);
+    }
+
+    public override string ToString()
+    {
+        return $"({x}, {y})";
+    }
+
+}
+
+
+public class Vector2I
+{
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public Vector2I(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float Magnitude()
+    {
+        return (float)Math.Sqrt(x * x + y * y);
+    }
+
+    public void Normalize()
+    {
+        float magnitude = Magnitude();
+        if (magnitude > 0)
+        {
+            x = (int)MathF.Round(x / magnitude);
+            y = (int)MathF.Round(y / magnitude);
+        }
+    }
+
+    public static Vector2I operator +(Vector2I a, Vector2I b)
+    {
+        return new Vector2I(a.x + b.x, a.y + b.y);
+    }
+
+    public static Vector2I operator -(Vector2I a, Vector2I b)
+    {
+        return new Vector2I(a.x - b.x, a.y - b.y);
+    }
+
+    public static Vector2I operator *(Vector2I a, float scalar)
+    {
+        return new Vector2I((int)MathF.Round(a.x * scalar), (int)MathF.Round(a.y * scalar));
+    }
+
+    public static Vector2I operator *(Vector2I a, Vector2I scalar)
+    {
+        return new Vector2I(a.x * scalar.x, a.y * scalar.y);
+    }
+
+    public static Vector2I operator /(Vector2I a, float scalar)
+    {
+        if (scalar != 0)
+        {
+            return new Vector2I((int)MathF.Round(a.x / scalar), (int)MathF.Round(a.y / scalar));
+        }
+        throw new DivideByZeroException("Division by zero is not allowed.");
+    }
+
+    public static implicit operator System.Numerics.Vector2(Vector2I customVector)
+    {
+        return new System.Numerics.Vector2(customVector.x, customVector.y);
+    }
+
+    public static implicit operator Vector2I(System.Numerics.Vector2 customVector)
+    {
+        return new Vector2I((int)MathF.Round(customVector.X), (int)MathF.Round(customVector.Y));
+    }
+    public static implicit operator Vector2I(Vector2 customVector)
+    {
+        return new Vector2I((int)MathF.Round(customVector.x), (int)MathF.Round(customVector.y));
+    }
+
+    public override string ToString()
+    {
+        return $"({x}, {y})";
+    }
+
+}
